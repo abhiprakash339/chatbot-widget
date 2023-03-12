34eca0c16fe4e2389186fdecfec0685cd612317e
Abhishek Prakash <1289prakash@gmail.com>
2023-03-13 03:16:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  AnimatePresence,
+  motion,
+} from 'framer-motion';
 import './App.css';
+import { Close, Send,QuestionAnswer } from '@mui/icons-material';
+const BouncingDotsLoader = (props) => {
+  return (
+    <div style={{ width: "100%", margin: "5px 0" }}>
+      <div className="hover-widget-bot-message hover-widget-bouncing-loader" style={{ display: "flex" }}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+
+  );
+};
+
+const UserMessage = ({ message }) => {
+  return (
+    <div style={{ width: "100%", margin: "5px 0" }}>
+      <span className="hover-widget-user-message">{message}</span>
+    </div>
+  )
+}
+const BotMessage = ({ message }) => {
+  return (
+    <div style={{ width: "100%", margin: "5px 0" }}>
+      <span className="hover-widget-bot-message">{message}</span>
+    </div>
+  )
+}
+function HoverWidget(props) {
+  return (
+    <div className="hover-widget-logo-container" onClick={() => props.onClick()}>
+      <QuestionAnswer />
+    </div>
+  )
+}
+const HoverBox = (props) => {
+  const [chatData, setChatData] = React.useState([])
+  const [message, setMessage] = React.useState('')
+  const [loader, setLoader] = React.useState(false)
+  const inputRef = React.useRef()
+  const messageRef = React.useRef()
+
+  React.useEffect(()=>{
+    messageRef.current.scrollIntoView({ behavior: 'smooth' })
+  },[chatData,loader])
+
+  const getBotResponse = () => {
+    const userMessag = inputRef.current.value
+    setMessage(() => "")
+    setChatData((data) => [...data, { type: 'user', message: userMessag }])
+    setLoader(true)
+    setTimeout(() => {
+      setLoader(false)
+      setChatData((data) => [...data, { type: 'bot', message: userMessag }])
+      
+    }, 5000);
+
+  }
+
+  const allMessage = React.useMemo(() => {
+    return chatData.map((item, index) => item.type === 'user' ? <UserMessage key={index} message={item.message} /> : <BotMessage key={index} message={item.message} />)
+  }, [chatData])
+
+
+  return (
+    <div className="hover-widget-box-container"  >
+      <div className="hover-widget-box-header">
+        <span>Hover</span>
+        <button onClick={() => props.onClick(false)}><Close /></button>
+      </div>
+      <div className="hover-widget-box-message">
+        {allMessage}
+        {loader ? <BouncingDotsLoader /> : ""}
+        <div ref={messageRef} />
+      </div>
+      <div className="hover-widget-box-input">
+        <textarea ref={inputRef} value={message} onChange={(e) => setMessage(e.target.value)} wrap="soft"></textarea>
+        <button onClick={() => getBotResponse()} disabled={loader}><Send /></button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [showMessage, setShowMessage] = React.useState(false);
+  const defaultState = {
+    opacity: 0,
+    scale: 0.2,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence>
+      {
+        showMessage === false ?
+          <HoverWidget onClick={() => setShowMessage(true)} />
+          :
+          <motion.div
+            initial={defaultState}
+            exit={defaultState}
+            transition={{ duration: 0.5 }}
+            animate={{
+              boxShadow: '1px 1px 10px rgba(0, 0, 0, 0.3)',
+              scale: 1,
+              opacity: 1
+            }}
+          >
+            <HoverBox onClick={setShowMessage} />
+          </motion.div>
+
+      }
+
+    </AnimatePresence>
   );
 }
 
